78afdd7a036cffcc05e344ab35aaf2d42c28e540
rudreshmahajan <rudreshmahajan43@gmail.com>
2021-07-30 18:03:16 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -18,24 +18,19 @@ function MyRegisterComponent() {
   ]);
 
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  co
   const [mobile, setMobile] = useState("");
 
 
   const usernameChangeHandler = (e) => setUsername(e.target.value);
-  const passwordChangeHandler = (e) => setPassword(e.target.value);
-  const emailChangeHandler = (e) => setEmail(e.target.value);
+
   const mobileChangeHandler = (e) => setMobile(e.target.value);
 
 
   const addNewUser = () => {
     const newuser = {
       id: userList.length + 1,
-      name: username,
-      password: password,
-      email,
-      mobile,
+     
     };
 
   }
